e395f38ac7cb13afc45b130f71a2eb39c89d52b2
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-07-30 06:02:02 +0530

    test code

---
+++ Solution/OtherTests/Triangle.cs
@@ -0,0 +1,24 @@
+﻿namespace OtherTests
+{
+    public class Triangle
+    {
+        private readonly int _base;
+        private readonly int _height;
+
+        public Triangle(int @base, int height)
+        {
+            _base = @base;
+            _height = height;
+        }
+
+        public int CalculateArea()
+        {
+            return (_base * _height) / 2;
+        }
+
+        public string AsString()
+        {
+            return $"Base is {_base}, height is {_height}";
+        }
+    }
+}
