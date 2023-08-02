3b7cbd1ccf1b7e92c4d95a1743140e0f962f15f1
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-02 12:01:11 +0530

    example struct - sperate file

---
+++ Solution/OtherTests/Point.cs
@@ -0,0 +1,45 @@
+﻿namespace OtherTests;
+
+public struct Point
+{
+    private float _x;
+    private float _y;
+
+    public Point()
+    {
+        _x = 0f;
+        _y = 0f;
+
+    }
+
+    public Point(float x, float y)
+    {
+        this._x = x;
+        this._y = y;
+    }
+
+    // + to go right
+    // - to go left
+    public void MoveRight(float right)
+    {
+        _x += right;
+    }
+
+    // + to go up
+    // - to go down
+    public void MoveUp(float up)
+    {
+        _y += up;
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine(ToString());
+    }
+
+    public override string ToString()
+    {
+        return $"Point: x => {_x} , y => {_y}";
+    }
+}
--- Solution/OtherTests/Program.cs
+++ Solution/OtherTests/Program.cs
@@ -1,49 +1,7 @@
-﻿var point = new Point();
+﻿using OtherTests;
+
+var point = new Point();
 point.Display();
 point.MoveRight(2);
 point.MoveUp(4);
 point.Display();
-
-struct Point
-{
-    private float x;
-    private float y;
-
-    public Point()
-    {
-        x = 0f;
-        y = 0f;
-
-    }
-
-    public Point(float x, float y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-
-    // + to go right
-    // - to go left
-    public void MoveRight(float right)
-    {
-        x += right;
-    }
-
-    // + to go up
-    // - to go down
-    public void MoveUp(float up)
-    {
-        y += up;
-    }
-
-
-    public void Display()
-    {
-        Console.WriteLine(ToString());
-    }
-
-    public override string ToString()
-    {
-        return $"Point: x => {x} , y => {y}";
-    }
-}
