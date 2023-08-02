76afd1d0c44caebbd2bfa470b0ec7c5ef62587ad
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-02 10:11:29 +0530

    example static class

---
+++ Solution/OtherTests/NumberToDayOfWeekTranslator.cs
@@ -0,0 +1,20 @@
+﻿namespace OtherTests
+{
+    public static class NumberToDayOfWeekTranslator
+    {
+        public static string Translate(int num)
+        {
+            switch (num)
+            {
+                case 1: return "Monday";
+                case 2: return "Tuesday";
+                case 3: return "Wednesday";
+                case 4: return "Thursday";
+                case 5: return "Friday";
+                case 6: return "Saturday";
+                case 7: return "Sunday";
+                default: return "Invalid day of the week";
+            }
+        }
+    }
+}
