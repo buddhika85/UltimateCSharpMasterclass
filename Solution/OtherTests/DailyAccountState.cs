d3fdb5a5d163ced5cff000343a41822fc7092292
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-02 07:15:17 +0530

    some test code

---
+++ Solution/OtherTests/DailyAccountState.cs
@@ -0,0 +1,22 @@
+﻿namespace OtherTests
+{
+    public class DailyAccountState
+    {
+        public int InitialState { get; }
+
+        public int SumOfOperations { get; }
+
+        public DailyAccountState(
+            int initialState,
+            int sumOfOperations)
+        {
+            InitialState = initialState;
+            SumOfOperations = sumOfOperations;
+        }
+
+        public int EndOfDayState => InitialState + SumOfOperations;
+
+        public string Report =>
+            $"Day: {DateTime.Today.Day}, month: {DateTime.Today.Month}, year: {DateTime.Today.Year}, initial state: {InitialState}, end of day state: {EndOfDayState}";
+    }
+}
---
+++ Solution/OtherTests/Order.cs
@@ -0,0 +1,24 @@
+﻿namespace OtherTests
+{
+    public class Order
+    {
+        public string Item { get; }
+
+        private DateTime _date;
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                if (value.Year == DateTime.Now.Year)
+                    _date = value;
+            }
+        }
+
+        public Order(string item, DateTime date)
+        {
+            Item = item;
+            Date = date;
+        }
+    }
+}
