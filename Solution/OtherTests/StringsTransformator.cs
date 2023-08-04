3b0173844d89de22e8e5263fadc318012db9ffd6
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-05 04:48:59 +0530

    some tests

---
+++ Solution/OtherTests/StringsTransformator.cs
@@ -0,0 +1,15 @@
+﻿namespace OtherTests
+{
+    public static class StringsTransformator
+    {
+        public static string TransformSeparators(
+            string input,
+            string originalSeparator,
+            string targetSeparator)
+        {
+            if (string.IsNullOrWhiteSpace(input) || string.IsNullOrWhiteSpace(originalSeparator) || string.IsNullOrWhiteSpace(targetSeparator))
+                return input;
+            return input.Replace(originalSeparator, targetSeparator);
+        }
+    }
+}
