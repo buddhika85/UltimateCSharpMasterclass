c0236907b92c21e01f96c9ca83487183a9731253
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-05 06:06:29 +0530

    Dice class added

---
+++ Solution/DiceRollGame/Dice.cs
@@ -0,0 +1,13 @@
+﻿namespace DiceRollGame
+{
+    public class Dice
+    {
+        private const int Min = 1;
+        private const int Max = 6;
+        public int Value { get; private set; }
+        public void Roll()
+        {
+            Value = new Random().Next(Min, Max + 1);
+        }
+    }
+}
