b2175dda2f3d68e65ecf4fae9933478f13add97b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-09 19:04:21 +0530

    some testing with new contraint

---
+++ Solution/OtherTests/LazyInitializerTesting/LazyInitializer.cs
@@ -0,0 +1,39 @@
+﻿namespace OtherTests.LazyInitializerTesting
+{
+    // new() puts constrain that any class that is used to 
+    // replace T has to have a parameterless constructor
+    public class LazyInitializer<T> where T : new()
+    {
+        private T _value;
+
+        public T Get()
+        {
+            if (_value == null)
+            {
+                _value = new T();
+            }
+            return _value;
+        }
+    }
+
+    public class Pet
+    {
+        public Pet() { }
+    }
+
+    public class Dog
+    {
+        public Dog(string breed) { }
+    }
+
+    public class Test
+    {
+        public Test()
+        {
+            Pet pet = new LazyInitializer<Pet>().Get();
+
+            // below do not compile because dog do not have parameterless constructor 
+            ////Dog dog = new LazyInitializer<Dog>().Get();
+        }
+    }
+}
