e28bd016067b1b6ba15ee7a9cc46215d66eef0f6
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-09 07:39:27 +0530

    testing virtual and override

---
+++ Solution/OtherTests/InheritanceAndPolymorphism/Parent.cs
@@ -0,0 +1,30 @@
+﻿namespace OtherTests.InheritanceAndPolymorphism
+{
+    public class Parent
+    {
+        public int X { get; set; }
+        public virtual int Y { get; set; }
+        public virtual int Z { get; set; }
+    }
+
+    public class Child : Parent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public override int Z { get; set; }
+    }
+
+    public class Test
+    {
+        public Test()
+        {
+            var parent = new Parent { X = 1, Y = 2, Z = 3 };
+            var child = new Child { X = 10, Y = 20, Z = 30 };
+            Parent childTwo = new Child { X = 10, Y = 20, Z = 30 };
+
+            Console.WriteLine($"Parent X = {parent.X}, Y = {parent.Y}, Z = {parent.Z}");
+            Console.WriteLine($"Child X = {child.X}, Y = {child.Y}, Z = {child.Z}");
+            Console.WriteLine($"ChildTwo X = {childTwo.X}, Y = {childTwo.Y}, Z = {childTwo.Z}");
+        }
+    }
+}
--- Solution/OtherTests/Program.cs
+++ Solution/OtherTests/Program.cs
@@ -1,7 +1,10 @@
-﻿using OtherTests;
+﻿
+//var point = new Point();
+//point.Display();
+//point.MoveRight(2);
+//point.MoveUp(4);
+//point.Display();
 
-var point = new Point();
-point.Display();
-point.MoveRight(2);
-point.MoveUp(4);
-point.Display();
+using OtherTests.InheritanceAndPolymorphism;
+
+new Test();
