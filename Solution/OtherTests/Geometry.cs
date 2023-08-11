4213f26f85579b3ef35d05993c0397822ebb5108
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-11 11:01:29 +0530

    using static using moidifier

---
+++ Solution/OtherTests/Geometry.cs
@@ -0,0 +1,18 @@
+﻿
+using static System.Math;
+
+public class Geometry
+{
+    public void SquareArea(double side)
+    {
+        var area = Pow(side, 2);
+        Console.WriteLine($"Square area is: {area}");
+    }
+
+    public void CircleArea(double radius)
+    {
+        var area = PI * Pow(radius, 2);
+        Console.WriteLine($"Circle area is: {area}");
+    }
+}
+
