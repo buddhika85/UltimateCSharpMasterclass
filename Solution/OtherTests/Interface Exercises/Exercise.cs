846729b4c9edc6410a4c000f0748414e9d200f37
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-26 05:49:59 +0530

    interfaces

---
+++ Solution/OtherTests/Interface Exercises/Exercise.cs
@@ -0,0 +1,52 @@
+﻿namespace OtherTests.Interface_Exercises
+{
+    public static class Exercise
+    {
+        public static int Transform(
+            int number)
+        {
+            var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+
+            var result = number;
+            foreach (var transformation in transformations)
+            {
+                result = transformation.Transform(result);
+            }
+            return result;
+        }
+    }
+
+    interface INumericTransformation
+    {
+        int Transform(int number);
+    }
+
+    public class By1Incrementer : INumericTransformation
+    {
+        public int Transform(int number)
+        {
+            return ++number;
+        }
+    }
+
+    public class By2Multiplier : INumericTransformation
+    {
+        public int Transform(int number)
+        {
+            return number * 2;
+        }
+    }
+
+    public class ToPowerOf2Raiser : INumericTransformation
+    {
+        public int Transform(int number)
+        {
+            return number * number;
+        }
+    }
+}
