1f87b36beec65f0864b5039d9ee06f92b8ab4b1f
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-30 19:11:21 +0530

    High level design - do not compile

---
+++ Solution/CookieCookbookModelAnswer/Program.cs
@@ -0,0 +1,46 @@
+﻿var cookieRecipesApp = new CookieRecipesApp();
+cookieRecipesApp.Run();
+
+public class CookieRecipesApp
+{
+    private readonly RecipesRepository _recipesRepository;
+    private readonly RecipesUserInteraction _recipesUserInteraction;
+
+    public CookieRecipesApp(RecipesRepository recipesRepository, RecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    public void Run()
+    {
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+        _recipesRepository.PromptToCreateRecipe();
+
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+        if (ingredients.Count > 0)
+        {
+            var recipe = new Recipe(ingredients);
+            allRecipes.Add(recipe);
+            _recipesRepository.Write(filePath, allRecipes);
+            _recipesUserInteraction.ShowMessage("Recipe added");
+            _recipesUserInteraction.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _recipesUserInteraction.ShowMessage(
+                "No ingredients have been selected. Recipe will not be saved."
+                );
+        }
+    }
+}
+
+public class RecipesUserInteraction
+{
+}
+
+public class RecipesRepository
+{
+}
