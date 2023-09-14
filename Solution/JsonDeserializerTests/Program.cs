9754c7ddbda95e468ccc4c6f26ce97fe142fb26a
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-14 22:10:18 +0530

    JSON deserialization from an API

--- Solution/JsonDeserializerTests/Program.cs
+++ Solution/JsonDeserializerTests/Program.cs
@@ -1,8 +1,60 @@
 ﻿using JsonDeserializerTests.Models;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 //ReadStaticJson();
-await ReadFromWeatherApi();
+//await ReadFromWeatherApi();
+await ReadFromWeatherApiUsingExtensionMethods();
+//await ReadFromWeatherApiUsingJsonDocument();
+
+async Task ReadFromWeatherApiUsingJsonDocument()
+{
+    using HttpClient client = new()
+    {
+        BaseAddress = new Uri("https://localhost:7241")
+    };
+    var response = await client.GetAsync("weatherforecast");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var jsonString = await response.Content.ReadAsStringAsync();
+        using (var jsonDocument = JsonDocument.Parse(jsonString))
+        {
+            var root = jsonDocument.RootElement;
+            //Console.WriteLine($"{root.ValueKind}");
+            foreach (var temperature in root.EnumerateArray())
+            {
+                Console.WriteLine(temperature.GetProperty("Summary").ToString());
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Whoops ! Error: {response.StatusCode}");
+    }
+}
+
+async Task ReadFromWeatherApiUsingExtensionMethods()
+{
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    using HttpClient client = new()
+    {
+        BaseAddress = new Uri("https://localhost:7241")
+    };
+
+    var temperatures = await client.GetFromJsonAsync<Temperature[]>("weatherforecast", options);
+    if (temperatures == null) return;
+
+    foreach (var temperature in temperatures)
+    {
+        Console.WriteLine($"{temperature.Summary}");
+    }
+}
+
 
 async Task ReadFromWeatherApi()
 {
