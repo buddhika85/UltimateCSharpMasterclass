b2f895258671c48dcfd6d7f77e9a8edeb690bf49
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-09-14 16:08:45 +0530

    json serirlization

---
+++ Solution/JsonSerializer/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using JsonSerializerTests.Models;
+using System.Text.Json;
+
+var person = new Person
+{
+    Id = 1,
+    FirstName = "Sean",
+    LastName = "Connery",
+    Age = 90,
+    IsAlive = false
+};
+
+var jsonFilePath = "D:\\buddhika\\projects\\C#Practise\\UltimateCSharpMasterclass\\Solution\\JsonSerializer\\person.json";
+var jsonString = JsonSerializer.Serialize(person);
+File.WriteAllText(jsonFilePath, jsonString);
+
+Console.WriteLine(File.ReadAllText(jsonFilePath));
