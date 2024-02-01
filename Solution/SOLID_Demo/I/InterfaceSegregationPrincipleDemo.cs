1c6f0a1db6fdb3a56140ca3c019e49cc2356ac37
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-01 17:31:06 +0530

    Interface Segregation Principle

---
+++ Solution/SOLID_Demo/I/InterfaceSegregationPrincipleDemo.cs
@@ -0,0 +1,50 @@
+﻿using static System.Console;
+
+namespace SOLID_Demo.I
+{
+    public class InterfaceSegregationPrincipleDemo
+    {
+    }
+
+    public interface IBike
+    {
+        void Ride();
+        void InflateTheTyre();
+    }
+
+    public interface IChargeable
+    {
+        void Charge();
+    }
+
+    public class Bike : IBike
+    {
+        public void Ride()
+        {
+            WriteLine("Use your muscles to move forward");
+        }
+
+        public void InflateTheTyre()
+        {
+            WriteLine("Use the pump to inflate the tyre.");
+        }
+    }
+
+    public class ElectricBike : IBike, IChargeable
+    {
+        public void Ride()
+        {
+            WriteLine("Use the electric motor to move forward");
+        }
+
+        public void InflateTheTyre()
+        {
+            WriteLine("Use the pump to inflate the tyre.");
+        }
+
+        public void Charge()
+        {
+            WriteLine("Charging the battery of electric bike.");
+        }
+    }
+}
