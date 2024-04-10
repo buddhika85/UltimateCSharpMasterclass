53caf14e65d28b2b91b6555ce2b9c03a2658ff29
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-11 04:22:59 +0530

    linq e

---
+++ Solution/LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnyWordWhiteSpace(new List<string> { }));       // f
+            Console.WriteLine(IsAnyWordWhiteSpace(new List<string> { "hello", "There    " } ));     // f
+            Console.WriteLine(IsAnyWordWhiteSpace(new List<string> { "hello", "    " }));           // true
+        }
+
+        public static bool IsAnyWordWhiteSpace(List<string> words)
+        {
+            return words.Any(word => word.All(c => char.IsWhiteSpace(c)));
+        }
+    }
+}
