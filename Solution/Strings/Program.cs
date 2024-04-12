71b9c1dbd2c69ff90c38c9b5a77d31e549f2bc23
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-12 10:15:49 +0530

    String vs StringBuilder Performace comparison

--- Solution/Strings/Program.cs
+++ Solution/Strings/Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System.Diagnostics;
+using System.Text;
+using static System.Console;
 
 namespace Strings
 {
@@ -6,7 +8,33 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            CharLoopDemo();
+            //CharLoopDemo();
+            //StringVsStringBuilder();
+
+            WriteLine(Reverse("Hello"));
+        }
+
+        private static void StringVsStringBuilder()
+        {
+            int count = 200_000;
+            String result = String.Empty;
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 1; i < count; i++)
+            {
+                result += "a";
+            }
+            sw.Stop();
+            WriteLine($"String {count} concatenations : {sw.ElapsedMilliseconds}");
+
+            // string builder
+            StringBuilder stringBuilder = new StringBuilder();
+            sw = Stopwatch.StartNew();
+            for (int i = 1; i < count; i++)
+            {
+                stringBuilder.Append("a");
+            }
+            sw.Stop();
+            WriteLine($"String Builder {count} appends: {sw.ElapsedMilliseconds}");
         }
 
         private static void CharLoopDemo()
@@ -15,6 +43,19 @@ namespace Strings
             {
                 Write($"{c}, ");
             }
+        }
+
+        private static string Reverse(string input)
+        {
+            if (input == "")
+                return input;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--) 
+            {
+                sb.Append(input[i]);
+            }
+            return sb.ToString();
         }
     }
 }
