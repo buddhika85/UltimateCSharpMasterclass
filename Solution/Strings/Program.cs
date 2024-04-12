921edb490712c92633c4eaf18b11f1358d4c8d59
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-12 08:11:44 +0530

    char loop

---
+++ Solution/Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CharLoopDemo();
+        }
+
+        private static void CharLoopDemo()
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Write($"{c}, ");
+            }
+        }
+    }
+}
