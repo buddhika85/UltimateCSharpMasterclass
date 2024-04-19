7c1e0633a5351f5f49ae6677bc5763afddc466dd
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-04-19 18:16:47 +0530

    single threaded example

--- Solution/Multithreading/Program.cs
+++ Solution/Multithreading/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Multithreading
+﻿using static System.Console;
+
+namespace Multithreading
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            PrintPluses(30);
+            PrintMinuses(30);
+
+            WriteLine($"\nCores count: {Environment.ProcessorCount}");
+
+
+            ReadKey();
+        }
+
+        private static void PrintPluses(int count)
+        {
+            WriteLine($"PrintPluses Thread ID : {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < count; i++)
+            {
+                Write("+");
+            }
+        }
+
+        private static void PrintMinuses(int count)
+        {
+            WriteLine($"\nPrintMinuses Thread ID : {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < count; i++)
+            {
+                Write("-");
+            }
         }
     }
 }
